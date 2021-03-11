a97112409bc16de2da2ced91967ebee32d1a6478
aasirvalji <aasir.valji@hotmail.com>
2021-03-10 21:43:07 -0500

    updated port

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.post('/sms', (req, res) => {
   res.end(twiml.toString());
 });
 
-http.createServer(app).listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+http.createServer(app).listen(PORT, () => {
   console.log('Express server listening on port 5000');
 });
